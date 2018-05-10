bff4a066ca39b3abae4607f6c08ba547edc2778f
Larry Tseng <lt222888@gmail.com>
2018-05-10 15:50:40 -0400

    Create deck.py

--- deck.py
+++ deck.py
@@ -9,24 +9,36 @@ class Deck(object):
     suits = ['s', 'h', 'd', 'c']
 
     def __init__(self):
+        """
+        Generates a deck of cards in reverse order (as, ks, qs...)
+        """
         self.deck = []
         for suit in self.suits:
-            for rank in reversed(self.ranks):
+            for rank in reversed(self.ranks):   # Iterates starting at the end
                 self.deck.append(rank + suit)
 
     def shuffle(self):
+        """
+        Shuffles the deck using the random library
+        """
         random.shuffle(self.deck)
 
     def deal(self, numCards):
+        """
+        Deals a specified number of cards from the deck
+        :param numCards: the number of cards to draw from the deck
+        :return: a list of drawn cards
+        """
         card = self.deck[:numCards]
         self.deck = self.deck[numCards:]
 
         return card
 
 
-random.seed(1)
-mydeck = Deck()
-print mydeck.deal(5)  # "should be ['as', 'ks', 'qs', 'js', 'ts']"
-mydeck.shuffle()
-print mydeck.deal(5)  # "should be ['3h', 'th', '7s', 'qc', '5c']"
-print mydeck.deal(2)  # "should be ['5h', '9h']"
+# TEST CASES
+# random.seed(1)
+# mydeck = Deck()
+# print mydeck.deal(5)  # "should be ['as', 'ks', 'qs', 'js', 'ts']"
+# mydeck.shuffle()
+# print mydeck.deal(5)  # "should be ['3h', 'th', '7s', 'qc', '5c']"
+# print mydeck.deal(2)  # "should be ['5h', '9h']"
